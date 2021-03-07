a9360cc9ba8b4c98f825650fb436a849f5b263b3
Giorno Giovanna <naufala47@gmail.com>
2021-03-07 22:06:09 +0700

    tambahan get

--- Controller/usersController.js
+++ Controller/usersController.js
@@ -52,6 +52,49 @@ exports.getUserByID = (req, res) => {
     })
 }
 
+exports.getUserByAddress = (req, res) => {
+    let userAddress = req.params.userAddress
+    DataUser.find({ userAddress: { $regex: userAddress, $options: "i" } }).exec((err, doc) => {
+        if (!err) {
+            res.status(200).json({
+                message: "GET berhasil",
+                data: doc
+            })
+        } else {
+            res.status(400).send("Gagal dapet data :" + err)
+        }
+    })
+}
+
+exports.getUserByEmail = (req, res) => {
+    let userEmail = req.params.userEmail
+    DataUser.find({ userEmail: { $regex: userEmail, $options: "i" } }).exec((err, doc) => {
+        if (!err) {
+            res.status(200).json({
+                message: "GET berhasil",
+                data: doc
+            })
+        } else {
+            res.status(400).send("Gagal dapet data :" + err)
+        }
+    })
+}
+
+exports.getUserByPhone = (req, res) => {
+    let userPhone = req.params.userPhone
+    DataUser.find({ userPhone: { $regex: userPhone, $options: "i" } }).exec((err, doc) => {
+        if (!err) {
+            res.status(200).json({
+                message: "GET berhasil",
+                data: doc
+            })
+        } else {
+            res.status(400).send("Gagal dapet data :" + err)
+        }
+    })
+}
+
+
 exports.deleteUser = (req, res) => {
     let idUser = req.params.id
     DataUser.findByIdAndDelete(idUser, (err, doc) => {
--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,11 @@ router.route('/:id')
   .get(usersController.getUserByID)
   .delete(usersController.deleteUser)
 
+router.route('/userEmail/:userEmail')
+  .get(usersController.getUserByEmail)
+router.route('/userPhone/:userPhone')
+  .get(usersController.getUserByPhone)
+router.route('/userAddress/:userAddress')
+  .get(usersController.getUserByAddress)
+
 module.exports = router;
